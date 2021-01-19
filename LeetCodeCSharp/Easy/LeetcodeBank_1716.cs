4e06b6f60077ec3b0c8ded29dad004b930b9d627
momoYin <ltboegod143@gmail.com>
2021-01-19 23:47:35 +0800

    Add [Easy] 1716.Leetcode Bank

---
+++ LeetCodeCSharp/Easy/LeetcodeBank_1716.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeCSharp
+{
+    class LeetcodeBank_1716
+    {
+        public int TotalMoney(int n)
+        {
+            int iAns = 0;
+
+            int iDivisor = n / 7;
+            int iRemainder = n % 7;
+
+            for (int i = 0; i < iDivisor; i++)
+            {
+                iAns += 28 + 7 * i;
+            }
+
+            for (int i = 1, iMondayMoney = iDivisor + 1; i <= iRemainder; i++, iMondayMoney++)
+            {
+                iAns += iMondayMoney;
+            }
+
+            return iAns;
+        }
+    }
+}
--- LeetCodeCSharp/Program.cs
+++ LeetCodeCSharp/Program.cs
@@ -9,11 +9,16 @@ namespace LeetCodeCSharp {
         static void Main(string[] args)
         {
             #region -- TwoSum_1 --
-            TwoSum_1 oTest = new TwoSum_1();
-            int[] nums = new int[4] { 2, 7, 11, 15 };
-            int target = 9;
-            int[] aAns = oTest.TwoSum(nums, target);
+            //TwoSum_1 oTest = new TwoSum_1();
+            //int[] nums = new int[4] { 2, 7, 11, 15 };
+            //int target = 9;
+            //int[] aAns = oTest.TwoSum(nums, target);
             #endregion
+
+            LeetcodeBank_1716 oTest = new LeetcodeBank_1716();
+            int iAns = oTest.TotalMoney(20);
+
+            
         }
     }
 }
